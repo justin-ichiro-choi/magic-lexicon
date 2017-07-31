d35085301aa2027c7ce4f26d07d845b2ae64ea6a
Justin Choi <justinch@uw.edu>
2017-07-30 17:56:07 -0700

    Added js stuff

---
+++ lexicon.js
@@ -0,0 +1,10 @@
+
+mtg.card.find(3)
+.then(result => {
+    console.log(result.card.name) // "Black Lotus"
+})
+
+mtg.set.find('AER')
+.then(result => {
+    console.log(result.set.name) // "Aether Revolt"
+})
